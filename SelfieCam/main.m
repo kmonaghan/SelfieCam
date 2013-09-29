7f2053ffca48ed76f738ca405f503c532cc58639
Karl Monaghan <karl.t.monaghan@gmail.com>
2013-09-29 16:46:28 +0100

    Removed temp fox for that warning that was all over the place

--- SelfieCam/main.m
+++ SelfieCam/main.m
@@ -10,24 +10,8 @@
 
 #import "CBPAppDelegate.h"
 
-#warning Temporary hack to remove the flood of assertmacro log messages.
-
-typedef int (*PYStdWriter)(void *, const char *, int);
-static PYStdWriter _oldStdWrite;
-
-int __pyStderrWrite(void *inFD, const char *buffer, int size)
-{
-    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
-        return 0;
-    }
-    return _oldStdWrite(inFD, buffer, size);
-}
-
 int main(int argc, char *argv[])
 {
-    _oldStdWrite = stderr->_write;
-    stderr->_write = __pyStderrWrite;
-    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([CBPAppDelegate class]));
     }
